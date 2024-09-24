bcb549ba8d07a766cdd02fc61691d1e4de4573e9
trung.nguyen <trung.nguyen@genefriendway.com>
2024-09-24 16:01:48 +0700

    add: membership_events.sql

--- sql/02-membership_events.sql
+++ sql/02-membership_events.sql
@@ -1,14 +1,16 @@
-CREATE TABLE reward (
-    id SERIAL PRIMARY KEY,  -- SERIAL takes care of auto-increment
-    reward_address VARCHAR(50) NOT NULL,
-    recipient_address VARCHAR(50) NOT NULL,
+CREATE TABLE membership_events (
+    id BIGSERIAL PRIMARY KEY,
+    user_address VARCHAR(50) NOT NULL,
+    order_id BIGINT NOT NULL,
     transaction_hash VARCHAR(66) NOT NULL,
-    token_amount NUMERIC(30, 18) NOT NULL, 
+    amount DECIMAL(30, 18),
     status SMALLINT NOT NULL DEFAULT 0,  -- 0 for pending, 1 for success, -1 for failed 
     error_message TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX membership_events_order_id_idx ON membership_events (order_id);
 
 -- Create a trigger to update 'updated_at' column on update
 CREATE OR REPLACE FUNCTION update_updated_at_column()
@@ -19,7 +21,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_reward_updated_at
-BEFORE UPDATE ON reward
+CREATE TRIGGER update_membership_events_updated_at
+BEFORE UPDATE ON membership_events
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
